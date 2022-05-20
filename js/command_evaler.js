f86391b464c378369ece2879358311a9bc1ed5d2
WowStarWorld <starworldstudio1@gmail.com>
2022-05-20 08:09:08 +0800

    1.0.7

--- js/command_evaler.js
+++ js/command_evaler.js
@@ -1,18 +1,22 @@
-function command_evaler(context={},command=""){
-    if (command.substring(0,2) == "//"){
-        return "";
+class commandEvaler{
+    constructor(context){
+        this.context = context;
     }
-    contexts = context;
-    command = command;
-    command_args = command.split(" ");
-    for (var i = 0; i < command_args.length; i++){
-        command_args[i] = command_args[i].replaceAll("<","&lt;").replaceAll(">","&gt;");
-    }
-    command_name = command_args[0];
-    command_args = command_args.slice(1,command_args.length);
-    if (contexts.hasOwnProperty(command_name)){
-        return contexts[command_name](...command_args);
-    }else{
-        throw ReferenceError("Command not found");
+    eval(command){
+        if (command.substring(0,2) == "//"){
+            return "";
+        }
+        this.command = command;
+        this.command_args = this.command.split(" ");
+        for (var i = 0; i < this.command_args.length; i++){
+            this.command_args[i] = this.command_args[i].replaceAll("<","&lt;").replaceAll(">","&gt;");
+        }
+        this.command_name = this.command_args[0];
+        this.command_args = this.command_args.slice(1,this.command_args.length);
+        if (this.context.hasOwnProperty(this.command_name)){
+            return this.context[this.command_name](...this.command_args);
+        }else{
+            throw ReferenceError("Command not found");
+        }
     }
 }
--- js/console.js
+++ js/console.js
@@ -1,6 +1,7 @@
 !(function(){
-    document.write("<script src=\"https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js\"></script>");
-    document.write("<script src=\"./js/command_evaler.js\"></script>");
+    document.write("<script src=\"https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js\"></script>"); // $
+    document.write("<script src=\"./js/command_evaler.js\"></script>"); // commandEvaler 
+    document.write("<script src=\"./js/context.js\"></script>"); // context
 })()
 
 //variables
@@ -8,8 +9,11 @@ var up = [""];
 var upnum = 0;
 var raw_console = console;
 var len =0;
-var version = "1.0.6";
+var version = "1.0.7";
 var code = "I am JavaScript Code";
+var help = ()=>{
+    return Object.keys(this);
+};
 
 console = {
     styles: {
@@ -113,129 +117,8 @@ window.onload = function(){
                     }
                 }
                 else{
-                    var context ={
-                        commands:{
-                            "help":function(command){
-                                if (command == undefined){
-                                    println("Usage: "+context.helps.help.usage,"color: #0f0;");
-                                    println("Commands: [ \""+Object.keys(context.helps).join("\" , \"")+"\" ]","color: #0f0;");
-                                }else if (command == "@all"){
-                                    for(var i = 0, len = Object.keys(context.helps).length; i < len; i++) {
-                                        names = (Object.keys(context.helps)[i]);
-                                        println(names,style="color: #0f0;");
-                                        println(`    Description: ${context.helps[names]["description"]}`,style="color: #bababa;")
-                                        println(`    Usage: ${context.helps[names]["usage"]}`,style="color: #bababa;")
-                                    }
-                                }
-                                else{
-                                    if (command in context.helps){
-                                        println(`${command}`,style="color: #0f0;");
-                                        println(`    Description: ${context.helps[command]["description"]}`,style="color: #bababa;")
-                                        println(`    Usage: ${context.helps[command]["usage"]}`,style="color: #bababa;")
-                                    }else{
-                                        throw ReferenceError("Command not found");
-                                    }
-                                }
-                                return "";
-                            },
-                            "args":function(...args){
-                                return args;
-                            },
-                            "js":function(...args){
-                                return eval(args.join(" "));
-                            },
-                            "console":function(sub,...args){
-                                if (typeof console[sub] == "function"){
-                                    return console[sub](...args);
-                                }
-                                else{
-                                    return console[sub];
-                                }
-                            },
-                            curl:function(url,method="GET",data="{}"){
-                                $.ajax({
-                                    url:url,
-                                    method:method,
-                                    data:JSON.parse(data.replaceAll("&nbsp;"," ")),
-                                    success:function(content){
-                                        println(content.replaceAll(">","&gt;").replaceAll("<","&lt;"),"color: #0f0;");
-                                    },
-                                    error:function(err){
-                                        println(JSON.stringify(err,null,2).replaceAll(">","&gt;").replaceAll("<","&lt;"),"color: #f00;");
-                                    }
-                                })
-                                
-                                return "";
-                            },
-                            import:function(url){
-                                $.ajax({
-                                    url:url,
-                                    success:function(content){
-                                        try{
-                                            return Function(content)();
-                                        }catch(err){
-                                            println("Unable to load module: \n"+err,"color: #f00;");
-                                        }
-                                    },
-                                    error:function(err){
-                                        println("Unable to get module: \n"+JSON.stringify(err,null,2).replaceAll("<","&lt;").replaceAll(">","&gt;"),"color: #f00;");
-                                    }
-                                })
-                                return "";
-                            },
-                            setvar:function(name,value){
-                                window[name] = value;
-                                return "";
-                            },
-                            getvar:function(name){
-                                return window[name];
-                            },
-                            eval:function(...code){
-                                return command_evaler(context["commands"],code.join(" "));
-                            }
-                        },
-                        helps:{
-                            help:{
-                                description:"Show this help message.",
-                                usage:"help | help [command] | help @all"
-                            },
-                            args:{
-                                description:"Show the arguments of the command.",
-                                usage:"args [*args]"
-                            },
-                            js:{
-                                description:"Execute javascript code.",
-                                usage:"js [*code]"
-                            },
-                            console:{
-                                description:"Output content on console",
-                                usage:`console [${Object.keys(console).join("|")}] [*args]`
-                            },
-                            curl:{
-                                description:"Execute ajax request.",
-                                usage:"curl [url] [method=GET] [data={}]"
-                            },
-                            import:{
-                                description:"Import a module.",
-                                usage:"import [url]"
-                            },
-                            setvar:{
-                                description:"Set a variable.",
-                                usage:"setvar [name] [value]"
-                            },
-                            getvar:{
-                                description:"Get a variable.",
-                                usage:"getvar [name]"
-                            },
-                            eval:{
-                                description:"Evaluate code.",
-                                usage:"eval [*code]"
-                            }
-
-                        }
-
-                    };
-                    result = String(command_evaler(context["commands"],v.replaceAll("<","&lt;").replaceAll(">","&gt;"))).replaceAll("\n","<br/>").replaceAll(" ","&nbsp;");
+                    var evaler = new commandEvaler(context["commands"]);
+                    result = String(evaler.eval(v.replaceAll("<","&lt;").replaceAll(">","&gt;"))).replaceAll("\n","<br/>").replaceAll(" ","&nbsp;");
                     if (String(result).replaceAll(" ","") != ""){
                         println("< "+`<span style='color: #9980f1;'>${result}</span>`+"","color: #bababa;");
                     }
